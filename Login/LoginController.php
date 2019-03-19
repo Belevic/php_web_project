f1ffceb2aac659868f701d32a498c0f8ea7eca94
Belevich Ilya <37711653+Belevic@users.noreply.github.com>
2019-03-19 21:33:36 +0300

    Update LoginController.php

--- Login/LoginController.php
+++ Login/LoginController.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace App\login;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+        $lastUsername = $utils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'username' => $lastUsername,
+            'error'         => $error
+        ]);
+    }
+}
