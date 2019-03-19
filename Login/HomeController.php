1ebd90886a3a9b205b5cb2a595d29569e57b3add
Belevich Ilya <37711653+Belevic@users.noreply.github.com>
2019-03-19 21:26:15 +0300

    Update HomeController.php

--- Login/HomeController.php
+++ Login/HomeController.php
@@ -1,1 +1,14 @@
+<?php
 
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    public function home()
+    {
+        return $this->render('index.html.twig');
+    }
+}
